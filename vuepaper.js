efc0d541a3d5b8b8e7d30ccedf7b76b6c5efdfa8
wei <mimi19910930@gmail.com>
2023-10-01 21:55:07 +0800

    更新

--- vuepaper.js
+++ vuepaper.js
@@ -6,7 +6,7 @@ const app = Vue.createApp({
       search: '',
       postList: [
 
-      {title:'大師系列講座10/11正式開講',url:'https://activity.ncku.edu.tw/index.php?c=apply&no=13840',img:'https://chass.ncku.edu.tw/var/file/11/1011/randimg/mobileadv_3538_7519992_27974.jpg',data:"2023.09.27"},
+      {title:'10/11大師系列講座開放報名',url:'https://activity.ncku.edu.tw/index.php?c=apply&no=13840',img:'https://chass.ncku.edu.tw/var/file/11/1011/randimg/mobileadv_3538_7519992_27974.jpg',data:"2023.09.27"},
     
       {title:'10/11(三)島鏈 x 歷史大師系列講座 ｜ 主講人：王汎森 院士',url:'	https://activity.ncku.edu.tw/index.php?c=apply&no=13840',img:'https://chass.ncku.edu.tw/var/file/11/1011/randimg/mobileadv_3538_7519992_27974.jpg',data:"2023.09.25"},
 
@@ -20,7 +20,7 @@ const app = Vue.createApp({
 
         {role:'計畫主持人',title:'蔡群立',url:'https://www.econ.ncku.edu.tw/Upload/NewsFiles/NewsPlusFiles/9/202301161525_52773.png',tel:"TEL：06-2757575#56051",email:'tchunli@ncku.edu.tw'},
       
-        {role:'計畫主持人',title:'徐珊惠',url:'https://pehl.ncku.edu.tw/p/412-1136-21994.php?Lang=zh-tw',tel:"TEL：06-2757575#56055",email:'tiffhsu@gs.ncku.edu.tw'},
+        {role:'共同主持人',title:'徐珊惠',url:'https://pehl.ncku.edu.tw/p/412-1136-21994.php?Lang=zh-tw',tel:"TEL：06-2757575#56055",email:'tiffhsu@gs.ncku.edu.tw'},
 
         {role:'共同主持人',title:'李啟彰',url:'https://his.ncku.edu.tw/p/412-1190-25944.php?Lang=zh-tw',tel:"TEL：06-2757575#52324",email:'	leechichang1911@yahoo.com.tw'},
 
@@ -39,13 +39,18 @@ const app = Vue.createApp({
         ],
       
       activityList: [
-      {title:'「一首歌，一個時代」音樂會',url:'https://activity.ncku.edu.tw/index.php?c=apply&no=13840',author:'蔡明叡、古意唸歌團',img:'https://chass.ncku.edu.tw/var/file/11/1011/img/4704/Poster_0926-F_-01.jpg',data:"2023.4.19",register:'活動已結束'},
       
-      {title:'「一首歌，一個時代」音樂會',url:'https://activity.ncku.edu.tw/index.php?c=apply&no=13840',author:'蔡明叡、古意唸歌團',img:'https://chass.ncku.edu.tw/var/file/11/1011/img/4704/Poster_0926-F_-01.jpg',data:"2023.4.19",register:'活動已結束'},
-      {title:'「一首歌，一個時代」音樂會',url:'https://activity.ncku.edu.tw/index.php?c=apply&no=13840',author:'蔡明叡、古意唸歌團',img:'https://chass.ncku.edu.tw/var/file/11/1011/img/4704/Poster_0926-F_-01.jpg',data:"2023.4.19",register:'活動已結束'},
-      {title:'「一首歌，一個時代」音樂會',url:'https://activity.ncku.edu.tw/index.php?c=apply&no=13840',author:'蔡明叡、古意唸歌團',img:'https://chass.ncku.edu.tw/var/file/11/1011/img/4704/Poster_0926-F_-01.jpg',data:"2023.4.19",register:'活動已結束'},
-      {title:'「一首歌，一個時代」音樂會',url:'https://activity.ncku.edu.tw/index.php?c=apply&no=13840',author:'蔡明叡、古意唸歌團',img:'https://chass.ncku.edu.tw/var/file/11/1011/img/4704/Poster_0926-F_-01.jpg',data:"2023.4.19",register:'活動已結束'},
-      {title:'「一首歌，一個時代」音樂會',url:'https://activity.ncku.edu.tw/index.php?c=apply&no=13840',author:'蔡明叡、古意唸歌團',img:'https://chass.ncku.edu.tw/var/file/11/1011/img/4704/Poster_0926-F_-01.jpg',data:"2023.4.19",register:'活動已結束'},
+      {title:'歷史Ｘ文學Ｘ生態讀書會',url:'activity.html',author:'--',img:'bookclub-06.jpg',data:"2023.9.16",register:'活動已結束'},
+      {title:'「從社會層面―考察思想史的意義」大師講座',url:'https://activity.ncku.edu.tw/index.php?c=apply&no=13840',author:'王汎森',img:'lecture-01.jpg',data:"2023.10.11",register:'開放報名中'},
+      {title:'文學主題讀書會',url:'activity.html',author:'陳文松、莫加南',img:'bookclub-08.jpg',data:"2023.10.11",register:'尚未開放報名'},
+      {title:'生態主題讀書會',url:'activity.html',author:'王毓正',img:'bookclub-09.jpg',data:"2023.10.13",register:'尚未開放報名'},
+      {title:'歷史主題讀書會',url:'activity.html',author:'沈玉慧',img:'bookclub-07.jpg',data:"2023.11.11",register:'尚未開放報名'},
+      
+      {title:'大師講座',url:'activity.html',author:'史書美',img:'lecture-03.jpg',data:"2023.12.5",register:'尚未開放報名'},
+      {title:'大師講座',url:'activity.html',author:'史書美',img:'lecture-04.jpg',data:"2023.12.6",register:'尚未開放報名'},
+      {title:'大師講座',url:'activity.html',author:'王汎森',img:'lecture-02.jpg',data:"2024.2.22",register:'尚未開放報名'},
+      {title:'大師講座',url:'activity.html',author:'陳玉峰',img:'lecture-05.jpg',data:"2023.3.5",register:'尚未開放報名'},
+      {title:'大師講座',url:'activity.html',author:'陳玉峰',img:'lecture-06.jpg',data:"2023.3.22",register:'尚未開放報名'},
      
       ],
 
@@ -62,9 +67,9 @@ const app = Vue.createApp({
     },
     computed: {
       filteredList() {
-        return this.postList.filter(post => {
+        return this.activityList.filter(post => {
           return post.title.toLowerCase().includes(this.search.toLowerCase())||
-          post.describe.toLowerCase().includes(this.search.toLowerCase());
+          post.author .toLowerCase().includes(this.search.toLowerCase());
           
         });
         
